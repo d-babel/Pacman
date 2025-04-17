810f32fecfcd8cd4eb943b28978df91b4e609db7
drewbabel <babeldrew@gmail.com>
2025-04-17 13:08:45 -0700

    corrected front/back ends

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
+import javax.swing.Timer;
+import java.awt.event.*;
 
-public class GameEngine {
+public class GameEngine implements ActionListener, KeyListener {
     private Pacman pacman;
     private ArrayList<Ghost> ghosts;
     private Maze maze;
     private int level;
     private boolean gameOver;
     private boolean gameWon;
+    private GamePanel panel;
+    private Timer timer;
 
     public GameEngine(int width, int height) {
         pacman = new Pacman(width / 2, height / 2, 30);
@@ -16,13 +20,31 @@ public class GameEngine {
         gameOver = false;
         gameWon = false;
     }
+    public GameEngine(int width, int height, GamePanel panel, int delay) {
+        this(width, height);
+        this.panel = panel;
+        timer = new Timer(delay, this);
+        timer.start();
+        panel.setFocusable(true);
+        panel.addKeyListener(this);
+    }
 
     public void update() {
         pacman.move();
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        update();
+        if (panel != null) {
+            panel.repaint();
+        }
+    }
 
     public void movePacman(int dx, int dy) {
         pacman.setDirection(dx, dy);
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
     }
 
     public Pacman getPacman() {
@@ -62,5 +84,24 @@ public class GameEngine {
     }
 
     public void nextLevel() {
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int dx = 0;
+        int dy = 0;
+        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+            dx = -1;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+            dx = 1;
+        } else if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+            dy = -1;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+            dy = 1;
+        }
+        movePacman(dx, dy);
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        movePacman(0, 0);
     }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,34 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
-public class GamePanel extends JPanel implements ActionListener, KeyListener {
+public class GamePanel extends JPanel {
     private int width;
     private int height;
     private int delay;
     private GameEngine engine;
-    private Timer timer;
 
     public GamePanel() {
         width = 600;
         height = 600;
         delay = 20;
-
-        engine = new GameEngine(width, height);
+        engine = new GameEngine(width, height, this, delay);
 
         setPreferredSize(new Dimension(width, height));
         setBackground(Color.BLACK);
-        setFocusable(true);
-        addKeyListener(this);
-
-        timer = new Timer(delay, this);
-        timer.start();
     }
 
-    public void actionPerformed(ActionEvent e) {
-        engine.update();
-        repaint();
-    }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -54,29 +42,6 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private void drawGameWon(Graphics g) {
     }
 
-    public void keyTyped(KeyEvent e) {
-    }
-
-    public void keyPressed(KeyEvent e) {
-        int dx = 0;
-        int dy = 0;
-
-        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
-            dx = -1;
-        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
-            dx = 1;
-        } else if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
-            dy = -1;
-        } else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
-            dy = 1;
-        }
-
-        engine.movePacman(dx, dy);
-    }
-
-    public void keyReleased(KeyEvent e) {
-        engine.movePacman(0, 0);
-    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Pac-Man Proof-of-Concept");
